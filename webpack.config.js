3b03b4a6211a0641f8c9a9134888ebd4917b7036
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-26 19:04:54 +0530

    update webpack configuration     add dev server config     add webpack build mode: set to 'development'

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 import path from "path";
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.js",
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
   },
 };
