62b02cc9a3418080dca7b0a2fd24ce52b9ad5487
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-28 08:04:53 +0530

    update getWeatherData function     add unit parameter

--- src/API/getWeatherData.js
+++ src/API/getWeatherData.js
@@ -1,8 +1,8 @@
 import api_key from "./api-key.json";
 
-export default async function getWeatherData({ lon, lat }) {
+export default async function getWeatherData({ lon, lat, unit }) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key.openWeatherAPIKey}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key.openWeatherAPIKey}&units=${unit}`
   );
 
   const weatherData = await response.json();
