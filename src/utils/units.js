190e25ebb6830cccbaa99ef6c44425e7a11b9ac3
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-28 17:20:31 +0530

    remove unit.json add units.js

---
+++ src/utils/units.js
@@ -0,0 +1,14 @@
+const units = {
+  IMPERIAL: {
+    name: "imperial",
+    temp_symbol: "F",
+    speed_symbol: "MPH",
+  },
+  METRIC: {
+    name: "metric",
+    temp_symbol: "C",
+    speed_symbol: "KMPH",
+  },
+};
+
+export default units;
