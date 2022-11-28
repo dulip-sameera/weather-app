3c01be11971c86ac94aba26cd18e44e1eb419a19
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-28 17:42:04 +0530

    update url

--- src/API/get_lat_and_lon_from_city_name.js
+++ src/API/get_lat_and_lon_from_city_name.js
@@ -3,7 +3,7 @@ import api_key from "./api-key.json";
 export default async function get_lat_and_lon_from_city_name(city_name) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${api_key.openWeatherAPIKey}`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${api_key.openWeatherAPIKey}`
     );
 
     const data = await response.json();
